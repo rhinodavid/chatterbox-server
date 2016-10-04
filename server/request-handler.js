2d5c93ad6d2e8693a42a7dcb05c9890174825ac9
rhinodavid <dawalsh@gmail.com>
2016-10-03 19:41:23 -0700

    Refactor

--- server/request-handler.js
+++ server/request-handler.js
@@ -3,8 +3,17 @@
 const url = require('url');
 const fs = require('fs');
 var defaultCorsHeaders;
-
 var chats = [];
+
+var Message = function(body) {
+  this.text = body.text;
+  this.username = body.username || 'User';
+  this.roomname = body.roomname || 'lobby';
+  this.createdAt = new Date();
+  this.updatedAt = this.createdAt;
+  this.objectId = chats.length;
+};
+
 
 var getContentType = function(pathname) {
   var types = {
@@ -27,66 +36,43 @@ var requestHandler = function(request, response) {
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
   const parsedUrl = url.parse(request.url);
-  let statusCode;
-  let headers;
+  let headers = defaultCorsHeaders;
   if (parsedUrl.pathname === '/classes/messages' && request.method === 'GET') {
-    // return saved messages
-    statusCode = 200;
-    headers = defaultCorsHeaders;
     headers['Content-Type'] = 'application/json';
-    response.writeHead(statusCode, headers);
+    response.writeHead(200, headers);
     response.end(JSON.stringify({results: chats}));
   } else if (parsedUrl.pathname === '/classes/messages' && request.method === 'POST') {
-    statusCode = 201;
-    headers = defaultCorsHeaders;
     headers['Content-Type'] = 'application/json';
-    response.writeHead(statusCode, headers);
+    response.writeHead(201, headers);
     var body = [];
-    request.on('data', function(chunk) {
+    request.on('data', chunk => {
       body.push(chunk);
     });
-    request.on('end', function() {
-      body = body.join('');//Buffer.concat(body).toString();
-      //Create a message object
+    request.on('end', () => {
+      body = body.join('');
       body = JSON.parse(body);
-      var message = {};
-      message.text = body.text;
-      message.username = body.username || 'User';
-      message.roomname = body.roomname || 'lobby';
-      message.createdAt = new Date();
-      message.updatedAt = message.createdAt;
-      message.objectId = chats.length;
-      chats.unshift(message);
-      response.end(JSON.stringify({createdAt: message.createdAt, objectId: message.objectId}));
+      chats.unshift(new Message(body));
+      response.end(JSON.stringify({createdAt: chats[0].createdAt, objectId: chats[0].objectId}));
     });
   } else if (parsedUrl.pathname === '/classes/messages' && request.method === 'OPTIONS') {
-    statusCode = 200;
-    headers = defaultCorsHeaders;
-    response.writeHead(statusCode, headers);
+    response.writeHead(200, headers);
     response.end();
   } else if (request.method === 'GET') {
-    debugger;
     var path = parsedUrl.pathname === '/' ? './client/client/index.html' : './client/client' + parsedUrl.pathname;
     fs.readFile(path, (err, data) => {
       if (err) {
-        statusCode = 404;
-        headers = defaultCorsHeaders;
         headers['Content-Type'] = 'text/plain';
-        response.writeHead(statusCode, headers);
+        response.writeHead(404, headers);
         response.end('404: File not found');
       } else {
-        statusCode = 200;
-        headers = defaultCorsHeaders;
         headers['Content-Type'] = getContentType(path) || 'text/plain';
-        response.writeHead(statusCode, headers);
+        response.writeHead(200, headers);
         response.end(data);
       }
     });
   } else {
-    statusCode = 404;
-    headers = defaultCorsHeaders;
     headers['Content-Type'] = 'text/plain';
-    response.writeHead(statusCode, headers);
+    response.writeHead(404, headers);
     response.end('404: Endpoint not found.');
   }
 
